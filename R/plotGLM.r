080bd354b7052598c974ded87d9c55b2fce133a4
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-07-18 16:21:40 +0100

    persistent bug III - the saga continues

--- R/plotGLM.r
+++ R/plotGLM.r
@@ -112,7 +112,6 @@ plotSimpleGLM <- function(Names, Log, probZero = .5, BF = 3, intercept = FALSE, 
     dev.off()
   }
 }
-
-test <- data.frame(read.table("~/Dropbox/Ebolavirus_Phylogeography/Location_GLM/BEAST GLM/LOGS/RE/EVD_counts_negbin_56L_GLM.log", header = TRUE))
-plotSimpleGLM(Names = paste("X_", 1:14, sep = ""),
-              Log = test, export = FALSE, intercept = TRUE)
+# test <- data.frame(read.table("~/Dropbox/Ebolavirus_Phylogeography/Location_GLM/BEAST GLM/LOGS/RE/EVD_counts_negbin_56L_GLM.log", header = TRUE))
+# plotSimpleGLM(Names = paste("X_", 1:14, sep = ""),
+#               Log = test, export = FALSE, intercept = TRUE)
