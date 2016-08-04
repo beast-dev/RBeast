d3460d38a737918231e29b56931de197e1d2aaf8
Marc Suchard <msuchard@gmail.com>
2016-08-04 15:45:14 -0700

    installed example files are always available

--- tests/testthat/test-readBeast2Trees.R
+++ tests/testthat/test-readBeast2Trees.R
@@ -1,11 +1,10 @@
 context("readBeast2Trees")
 
 test_that("readBeast2Trees: use", {
-  trees_file <- "../../inst/extdata/readBeast2TreesExample.trees"
-  if (file.exists(trees_file)) {
-	  posterior <- readBeast2Trees(trees_file)
-	  expect_equal(length(posterior), 10)
-	  expect_equal(class(posterior[[1]]), "phylo")
-  }
+	trees_file <- system.file("extdata", "readBeast2TreesExample.trees", package = "RBeast")
+	expect_true(file.exists(trees_file))
+	posterior <- readBeast2Trees(trees_file)
+	expect_equal(length(posterior), 10)
+	expect_equal(class(posterior[[1]]), "phylo")
 })
 
