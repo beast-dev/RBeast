7f8646b44bb1e42d740b05d2aed565202bc7d67b
Marc Suchard <msuchard@gmail.com>
2016-08-06 06:52:38 -0700

    Appease lintr

--- R/newCode.R
+++ R/newCode.R
@@ -1,192 +1,255 @@
 
+
 ######################################################################
 #### functions for package
 
-readInputData<- function(file, sep){
-	inputData<-  utils::read.csv(file, header=TRUE, stringsAsFactors=FALSE, sep=sep ,check.names=FALSE)
-	cat(paste("Have read table with",nrow(inputData),"lines and",ncol(inputData),"columns.\n"))
-	return(as.data.frame(inputData))
+readInputData <- function(file, sep) {
+  inputData <-
+    utils::read.csv(
+      file,
+      header = TRUE,
+      stringsAsFactors = FALSE,
+      sep = sep ,
+      check.names = FALSE
+    )
+  cat(paste(
+    "Have read table with",
+    nrow(inputData),
+    "lines and",
+    ncol(inputData),
+    "columns.\n"
+  ))
+  return(as.data.frame(inputData))
 }
 
 
-addTimeScaleToData<- function(iT, fT, data){
-	time<-  seq(iT, fT, length.out=nrow(data))
-	data<-  cbind(time=time, data)
-	cat(paste("Have added a time scale ",iT,"-",fT,"(",nrow(data),"steps )\n"))
-	return(data)
+addTimeScaleToData <- function(iT, fT, data) {
+  time <-  seq(iT, fT, length.out = nrow(data))
+  data <-  cbind(time = time, data)
+  cat(paste(
+    "Have added a time scale ",
+    iT,
+    "-",
+    fT,
+    "(",
+    nrow(data),
+    "steps )\n"
+  ))
+  return(data)
 }
 
 
 
 demo <- function() {
+  ######################################################################
+  #### user code
 
-######################################################################
-#### user code
+  #sorting out data
 
-	#sorting out data
+  filename <- "~/Desktop/Trinidad/LTT/fractionOfPopsize2.txt"
+  separator <- "\t"
+  itime <- 1920
+  ftime <- 2008
 
-		filename<- "~/Desktop/Trinidad/LTT/fractionOfPopsize2.txt"
-		separator<- "\t"
-		itime<- 1920
-		ftime<- 2008
+  data <-  readInputData(file = filename, sep = separator)
+  data <- addTimeScaleToData(iT = itime, fT = ftime, data = data)
+  nDataCols <-
+    ncol(data) - 1 #how many data columns we have, excluding time
 
-		data<-  readInputData(file=filename, sep=separator)
-		data<- addTimeScaleToData(iT= itime, fT= ftime, data=data)
-		nDataCols<- ncol(data)-1 #how many data columns we have, excluding time
+  # source("~/Desktop/Trinidad/LTT/plotStackedAreas.R")
 
-		# source("~/Desktop/Trinidad/LTT/plotStackedAreas.R")
+  #examples of plotting
 
-	#examples of plotting
+  #normal plot, little options
+  plotStackedAreas(data,
+                   stacked100 = FALSE,
+                   ylab = "variable",
+                   xlab = "time")
 
-		#normal plot, little options
-		plotStackedAreas(
-			data,
-			stacked100=FALSE,
-			ylab="variable",
-			xlab="time"
-		)
+  #stacked plot, little options
+  plotStackedAreas(data,
+                   stacked100 = TRUE,
+                   ylab = "relative variable",
+                   xlab = "time")
 
-		#stacked plot, little options
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time"
-		)
+  ### Load the package or install if not present
+  #     if (!require("RColorBrewer")) {
+  #       install.packages("RColorBrewer")
+  #       library(RColorBrewer)
+  #     }
 
-		### Load the package or install if not present
-# 		if (!require("RColorBrewer")) {
-# 		  install.packages("RColorBrewer")
-# 		  library(RColorBrewer)
-# 		}
-
-		#stacked plot, working with options
-		#myColours= brewer.pal(8,"Set1")
-    myColours= (grDevices::rainbow(nDataCols, start=0.11))
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours #use area colours to hide borders
-		)
+  #stacked plot, working with options
+  #myColours= brewer.pal(8,"Set1")
+  myColours = (grDevices::rainbow(nDataCols, start = 0.11))
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours #use area colours to hide borders
+  )
 
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=TRUE, #add white area around?
-			main="with axis margins"
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = TRUE,
+    #add white area around?
+    main = "with axis margins"
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		myOrder= sample(1:nDataCols, nDataCols) #random selection of data columns
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			order=myOrder, #choose order to plot data
-			main="with data using random order"
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  myOrder = sample(1:nDataCols, nDataCols) #random selection of data columns
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    order = myOrder,
+    #choose order to plot data
+    main = "with data using random order"
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			main="playing with legends", #add title
-			addLegend="topright" #add a legend to it by giving position to use
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    main = "playing with legends",
+    #add title
+    addLegend = "topright" #add a legend to it by giving position to use
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			main="playing with legends", #add title
-			addLegend="topright", #add a legend to it
-			legend.cex=0.85 #edit legend: change font size
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    main = "playing with legends",
+    #add title
+    addLegend = "topright",
+    #add a legend to it
+    legend.cex = 0.85 #edit legend: change font size
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			main="playing with legends", #add title
-			boxlwd=2, #add a nice border to the entire plot
-			addLegend="topright", #add a legend to it
-			legend.cex=0.85, #edit legend: change font size
-			legend.bg="white" #edit legend: bg colour
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    main = "playing with legends",
+    #add title
+    boxlwd = 2,
+    #add a nice border to the entire plot
+    addLegend = "topright",
+    #add a legend to it
+    legend.cex = 0.85,
+    #edit legend: change font size
+    legend.bg = "white" #edit legend: bg colour
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			main="playing with legends", #add title
-			boxlwd=2, #add a nice border to the entire plot
-			addLegend="topright", #add a legend to it
-			legend.cex=0.85, #edit legend: change font size
-			legend.bg="white", #edit legend: bg colour
-			file="myoutoutfilename", #choose to export this plot to a PDF file
-			pdfW=5.5, #choose width of PDF
-			pdfH=5	#choose height of PDF
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    main = "playing with legends",
+    #add title
+    boxlwd = 2,
+    #add a nice border to the entire plot
+    addLegend = "topright",
+    #add a legend to it
+    legend.cex = 0.85,
+    #edit legend: change font size
+    legend.bg = "white",
+    #edit legend: bg colour
+    file = "myoutoutfilename",
+    #choose to export this plot to a PDF file
+    pdfW = 5.5,
+    #choose width of PDF
+    pdfH = 5  #choose height of PDF
+  )
 
-		#stacked plot, working with options
-		myColours= grDevices::rainbow(nDataCols, start=0.11)
-		myColours[length(myColours)]= "white"
-		plotStackedAreas(
-			data,
-			stacked100=TRUE,
-			ylab="relative variable",
-			xlab="time",
-			colours= myColours, #choose colours
-			areaBorderCol= myColours, #use area colours to hide borders
-			addAxisSpace=FALSE, #add white area around?
-			main="final example", #add title
-			boxlwd=2, #add a nice border to the entire plot
-			addLegend="topright", #add a legend to it
-			legend.cex=0.85, #edit legend: change font size
-			legend.bg="white", #edit legend: bg colour
-			legend.pt.cex=1.5, #scalling of points in legend
-			file="myoutoutfilename", #choose to export this plot to a PDF file
-			pdfW=5.5, #choose width of PDF
-			pdfH=5	#choose height of PDF
-		)
+  #stacked plot, working with options
+  myColours = grDevices::rainbow(nDataCols, start = 0.11)
+  myColours[length(myColours)] = "white"
+  plotStackedAreas(
+    data,
+    stacked100 = TRUE,
+    ylab = "relative variable",
+    xlab = "time",
+    colours = myColours,
+    #choose colours
+    areaBorderCol = myColours,
+    #use area colours to hide borders
+    addAxisSpace = FALSE,
+    #add white area around?
+    main = "final example",
+    #add title
+    boxlwd = 2,
+    #add a nice border to the entire plot
+    addLegend = "topright",
+    #add a legend to it
+    legend.cex = 0.85,
+    #edit legend: change font size
+    legend.bg = "white",
+    #edit legend: bg colour
+    legend.pt.cex = 1.5,
+    #scalling of points in legend
+    file = "myoutoutfilename",
+    #choose to export this plot to a PDF file
+    pdfW = 5.5,
+    #choose width of PDF
+    pdfH = 5  #choose height of PDF
+  )
 
 }
--- R/plotStackedAreas.R
+++ R/plotStackedAreas.R
@@ -1,6 +1,6 @@
 
 .onLoad <- function(libname, pkgname) {
-	rJava::.jpackage(pkgname, lib.loc = libname)
+  rJava::.jpackage(pkgname, lib.loc = libname)
 }
 
 
@@ -22,107 +22,107 @@
 #' @export
 runBeast <- function(commandLine = "") {
   if (1 == 2) {
-	  strings <- rJava::.jarray(commandLine)
-	  rJava::J("dr.app.beast.RBeastMain")$main(strings)
+    strings <- rJava::.jarray(commandLine)
+    rJava::J("dr.app.beast.RBeastMain")$main(strings)
   }
 }
 
 plotStackedAreas <- function(
-	data,
-	stacked100=TRUE,
-	boxlwd=1.5,
-	order = NA,
-	ylab="", xlab="",
-	areaBorderWidth= NA,
-	areaBorderCol="black",
-	colours= NA,
-	addAxisSpace=FALSE,
-	addLegend=NA,
-	legend.cex=1,
-	legend.bg=NA,
-	main="",
-	file=NA,
-	pdfW=7,
-	pdfH=5,
-	legend.pt.cex=1,
-	...
+  data,
+  stacked100=TRUE,
+  boxlwd=1.5,
+  order = NA,
+  ylab="", xlab="",
+  areaBorderWidth= NA,
+  areaBorderCol="black",
+  colours= NA,
+  addAxisSpace=FALSE,
+  addLegend=NA,
+  legend.cex=1,
+  legend.bg=NA,
+  main="",
+  file=NA,
+  pdfW=7,
+  pdfH=5,
+  legend.pt.cex=1,
+  ...
 ){
 
-	#extract time scale
-	X= data$time
-	#normalize data to maximum? (stacked 100% plot)
-	if(stacked100){
-		sumDataCols<- function(X){
-			return(sum(X[2:length(X)]))
-		}
-		dataSums= apply(data, MARGIN=1, FUN=sumDataCols)
-		Y= data[,2:ncol(data)]/dataSums
-	}else{
-		Y= data[,2:ncol(data)]
-	}
-	ndatacols= ncol(Y)
+  #extract time scale
+  X= data$time
+  #normalize data to maximum? (stacked 100% plot)
+  if(stacked100){
+    sumDataCols<- function(X){
+      return(sum(X[2:length(X)]))
+    }
+    dataSums= apply(data, MARGIN=1, FUN=sumDataCols)
+    Y= data[,2:ncol(data)]/dataSums
+  }else{
+    Y= data[,2:ncol(data)]
+  }
+  ndatacols= ncol(Y)
 
-	#make some checks before it breaks
-	if(sum(Y<0)>=1) stop("Data columns can not have negative values.")
-	if(sum(X<0)>=1) stop("Time scale can not have negative values.")
-	if(nrow(Y)==0) stop("Data columns appear to be empty.")
-	if(length(X)==0) stop("Time scale appears to be empty.")
+  #make some checks before it breaks
+  if(sum(Y<0)>=1) stop("Data columns can not have negative values.")
+  if(sum(X<0)>=1) stop("Time scale can not have negative values.")
+  if(nrow(Y)==0) stop("Data columns appear to be empty.")
+  if(length(X)==0) stop("Time scale appears to be empty.")
 
-	#work on default parameters
-	if(is.na(areaBorderWidth)) areaBorderWidth<- 1
-	if(length(colours)==1){
-		if(is.na(colours)) colours <- grDevices::rainbow(ndatacols)
-	}
-	areaBorderCol <- as.vector(matrix(areaBorderCol, nrow=ndatacols, ncol=1))
-	colours <- as.vector(matrix(colours, nrow=ndatacols, ncol=1))
-	areaBorderWidth <- as.vector(matrix(areaBorderWidth, nrow=ndatacols, ncol=1))
+  #work on default parameters
+  if(is.na(areaBorderWidth)) areaBorderWidth<- 1
+  if(length(colours)==1){
+    if(is.na(colours)) colours <- grDevices::rainbow(ndatacols)
+  }
+  areaBorderCol <- as.vector(matrix(areaBorderCol, nrow=ndatacols, ncol=1))
+  colours <- as.vector(matrix(colours, nrow=ndatacols, ncol=1))
+  areaBorderWidth <- as.vector(matrix(areaBorderWidth, nrow=ndatacols, ncol=1))
 
-	if(length(order)>1) {
-		if(length(order)!=ndatacols) stop("Order array must be the length of data columns.")
-		Y <- Y[, order]
-		colours <- colours[order]
-		areaBorderWidth <- areaBorderWidth[order]
-		areaBorderCol<- areaBorderCol[order]
-	}
+  if(length(order)>1) {
+    if(length(order)!=ndatacols) stop("Order array must be the length of data columns.")
+    Y <- Y[, order]
+    colours <- colours[order]
+    areaBorderWidth <- areaBorderWidth[order]
+    areaBorderCol<- areaBorderCol[order]
+  }
 
-	upperPrevious <- X*0
-	areas <- vector(mode="list", ndatacols)
-	for(i in seq(areas)){
-		upperThis <- upperPrevious + Y[,i]
-		areas[[i]] <- list(x=c(X, rev(X)), y=c(upperPrevious, rev(upperThis)))
-		upperPrevious <- upperThis
-	}
+  upperPrevious <- X*0
+  areas <- vector(mode="list", ndatacols)
+  for(i in seq(areas)){
+    upperThis <- upperPrevious + Y[,i]
+    areas[[i]] <- list(x=c(X, rev(X)), y=c(upperPrevious, rev(upperThis)))
+    upperPrevious <- upperThis
+  }
 
-	xaxs="r"
-	yaxs="r"
-	if(!addAxisSpace){
-		xaxs="i"
-		yaxs="i"
-	}
+  xaxs="r"
+  yaxs="r"
+  if(!addAxisSpace){
+    xaxs="i"
+    yaxs="i"
+  }
 
-	if(!is.na(file)){
-		file=paste(file,".pdf",sep="")
-		grDevices::pdf(file, width = pdfW, height = pdfH, bg="white")
-	}
+  if(!is.na(file)){
+    file=paste(file,".pdf",sep="")
+    grDevices::pdf(file, width = pdfW, height = pdfH, bg="white")
+  }
 
-	graphics::layout(matrix(1, ncol=1, byrow=TRUE))
-	graphics::par(mar=c(4, 4, 2, 2), cex=0.9)
+  graphics::layout(matrix(1, ncol=1, byrow=TRUE))
+  graphics::par(mar=c(4, 4, 2, 2), cex=0.9)
 
-	ylim <- range(sapply(areas, function(x) range(x$y, na.rm=TRUE)), na.rm=TRUE)
-	graphics::plot(X,Y[,1], ylab=ylab, xlab=xlab, ylim=ylim, t="n", xaxs=xaxs, yaxs=yaxs, main=main)
-	for(i in seq(areas)){
-		graphics::polygon(areas[[i]], border=areaBorderCol[i], col=colours[i], lwd=areaBorderWidth[i])
-	}
+  ylim <- range(sapply(areas, function(x) range(x$y, na.rm=TRUE)), na.rm=TRUE)
+  graphics::plot(X,Y[,1], ylab=ylab, xlab=xlab, ylim=ylim, t="n", xaxs=xaxs, yaxs=yaxs, main=main)
+  for(i in seq(areas)){
+    graphics::polygon(areas[[i]], border=areaBorderCol[i], col=colours[i], lwd=areaBorderWidth[i])
+  }
 
-	if(!is.na(addLegend)){
-		names= colnames(Y)
-		graphics::legend(addLegend, legend=names, pch=22, col="black", pt.bg=colours, pt.cex=legend.pt.cex, pt.lwd=1, lwd=0, lty=NA, box.lwd=NA, bg=legend.bg, cex=legend.cex)
-	}
+  if(!is.na(addLegend)){
+    names= colnames(Y)
+    graphics::legend(addLegend, legend=names, pch=22, col="black", pt.bg=colours, pt.cex=legend.pt.cex, pt.lwd=1, lwd=0, lty=NA, box.lwd=NA, bg=legend.bg, cex=legend.cex)
+  }
 
-	graphics::box(lwd=boxlwd)
+  graphics::box(lwd=boxlwd)
 
-	if(!is.na(file)){
-		a <- grDevices::dev.off()
-	}
+  if(!is.na(file)){
+    a <- grDevices::dev.off()
+  }
 
 }
