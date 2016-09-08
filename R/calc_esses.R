2d478fda59a985f7d3e3165277952cbe4d805ab6
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-08 16:28:15 +0200

    Do not calculate the ESS from the timesteps at which the samples are taken

--- R/calc_esses.R
+++ R/calc_esses.R
@@ -1,7 +1,31 @@
-#' Calculates the Effective Sample Sizes
+#' Calculates the Effective Sample Sizes from a parsed BEAST2 log file
 #' @param traces a dataframe with traces with removed burn-in
 #' @param sample_interval the interval in timesteps between samples
 #' @return the effective sample sizes
+#' @examples
+#'
+#'   # Obtain a log file its name
+#'   filename <- system.file(
+#'     "extdata", "beast2_example_output.log", package = "RBeast"
+#'   )
+#'
+#'   # Parse that log file
+#'   beast_log_full <- parse_beast_log(filename = filename)
+#'
+#'   # Remove the burn-in
+#'   beast_log <- remove_burn_ins(
+#'     beast_log_full,
+#'     burn_in_fraction = 0.1
+#'   )
+#'
+#'   # Calculates the effective sample sizes of all parameter estimates
+#'   measured <- calc_esses(beast_log, sample_interval = 1000)
+#'
+#'   # Round off values to nearest integers
+#'   measured <- as.integer(measured[1, ] + 0.5)
+#'   expected <- c(10, 10, 10, 10, 7, 10, 9, 6)
+#'   testit::assert(all(measured == expected))
+#'
 #' @export
 #' @author Richel Bilderbeek
 calc_esses <- function(traces, sample_interval) {
@@ -11,6 +35,8 @@ calc_esses <- function(traces, sample_interval) {
   if (sample_interval < 1) {
     stop("sample interval must be at least one")
   }
+  # Remove the Sample column from the dataframe
+  traces <- subset(traces, select = -c(Sample ))
 
   esses <- rep(NA, ncol(traces))
 
--- tests/testthat/test-calc_esses.R
+++ tests/testthat/test-calc_esses.R
@@ -17,8 +17,8 @@ test_that("calc_esses: use", {
   )
   df <- calc_esses(estimates, sample_interval = 1000)
 
-  df_expected <- estimates[1, ]
-  df_expected[1, ] <- c(3, 10, 10, 10, 10, 7, 10, 9, 6)
+  df_expected <- df
+  df_expected[1, ] <- c(10, 10, 10, 10, 7, 10, 9, 6)
   df[1, ] <- as.integer(df[1, ] + 0.5) # Round off
 
 
