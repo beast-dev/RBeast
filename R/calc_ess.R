38c304f4b0bc56825dc95f3f5b18c245afed9628
richelbilderbeek <richel@richelbilderbeek.nl>
2017-07-21 12:13:48 +0200

    Added calc_ess example

--- R/calc_ess.R
+++ R/calc_ess.R
@@ -2,6 +2,36 @@
 #' @param trace the values without burn-in
 #' @param sample_interval the interval in timesteps between samples
 #' @return the effective sample size
+#' @examples
+#'   filename <- system.file(
+#'    "extdata", "beast2_example_output.log", package = "RBeast"
+#'  )
+#'
+#'  # Parse the file as-is and conclude the sampling interval
+#'  df <- RBeast::parse_beast_log(
+#'    filename = filename
+#'  )
+#'  sample_interval <- df$Sample[2] - df$Sample[1]
+#'
+#'  # Only keep the parameter estimates, do not care about the sampling times anymore
+#'  estimates <- subset(df, select = -Sample)
+#'
+#'  esses <- rep(NA, ncol(estimates))
+#'  burn_in_fraction <- 0.1
+#'  for (i in seq_along(estimates)) {
+#'    # Trace with the burn-in still present
+#'    trace_raw <- as.numeric(t(estimates[i]))
+#'
+#'    # Trace with the burn-in removed
+#'    trace <- RBeast::remove_burn_in(trace = trace_raw, burn_in_fraction = 0.1)
+#'
+#'    # Store the effectice sample size
+#'    esses[i] <- RBeast::calc_ess(trace, sample_interval = sample_interval)
+#'  }
+#'
+#'  # Use the values that TRACER shows
+#'  expected_esses <- c(10, 10, 10, 10, 7, 10, 9, 6)
+#'  testit::assert(all(expected_esses - esses < 0.5))
 #' @export
 #' @author Richel Bilderbeek
 calc_ess <- function(trace, sample_interval) {
--- tests/testthat/test-calc_ess.R
+++ tests/testthat/test-calc_ess.R
@@ -1,14 +1,19 @@
 context("calc_ess")
 
-test_that("calc_ess: use", {
+test_that("exanple usage", {
 
   filename <- system.file(
     "extdata", "beast2_example_output.log", package = "RBeast"
   )
 
-  estimates <- parse_beast_log(
+  # Parse the file as-is and conclude the sampling interval
+  df <- RBeast::parse_beast_log(
     filename = filename
   )
+  sample_interval <- df$Sample[2] - df$Sample[1]
+
+  # Only keep the parameter estimates, do not care about the sampling times anymore
+  estimates <- subset(df, select = -Sample)
 
   esses <- rep(NA, ncol(estimates))
   burn_in_fraction <- 0.1
@@ -17,17 +22,15 @@ test_that("calc_ess: use", {
     trace_raw <- as.numeric(t(estimates[i]))
 
     # Trace with the burn-in removed
-    trace <- remove_burn_in(trace = trace_raw, burn_in_fraction = 0.1)
+    trace <- RBeast::remove_burn_in(trace = trace_raw, burn_in_fraction = 0.1)
 
     # Store the effectice sample size
-    esses[i] <- calc_ess(trace, sample_interval = 1000)
+    esses[i] <- RBeast::calc_ess(trace, sample_interval = sample_interval)
   }
 
-  # Note that the first value of three is nonsense:
-  # it is the index of the sample. I keep it in
-  # for simplicity of writing this code
-  expected_esses <- c(3, 10, 10, 10, 10, 7, 10, 9, 6)
-  expect_true(all(expected_esses - esses < 0.5))
+  # Use the values that TRACER shows
+  expected_esses <- c(10, 10, 10, 10, 7, 10, 9, 6)
+  testthat::expect_true(all(expected_esses - esses < 0.5))
 })
 
 test_that("calc_ess: abuse", {
