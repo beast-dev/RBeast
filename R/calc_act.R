ebaae5ef744e04dc4ad18380cc1c26a42dfaa84c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-25 16:34:42 +0200

    Improve doc, increase codecov

--- R/calc_act.R
+++ R/calc_act.R
@@ -3,6 +3,13 @@
 #' @param trace the values
 #' @param sample_interval the interval in timesteps between samples
 #' @return the auto_correlation time
+#' @examples
+#'   trace <- sin(seq(from = 0.0, to = 2.0 * pi, length.out = 100))
+#'   act <- RBeast::calc_act_r(
+#'     trace = trace,
+#'     sample_interval = 1
+#'   )
+#'   testthat::expect_equal(object = act, expected = 38.18202, tolerance = 0.01)
 #' @export
 #' @seealso Java code can be found here: \url{https://github.com/CompEvol/beast2/blob/9f040ed0357c4b946ea276a481a4c654ad4fff36/src/beast/core/util/ESS.java#L161}
 #' @author The original Java version of the algorithm was from Remco Bouckaert,
@@ -90,6 +97,13 @@ NULL
 #' @param sample_interval the interval in timesteps between samples
 #' @return the auto_correlation time
 #' @export
+#' @examples
+#'   trace <- sin(seq(from = 0.0, to = 2.0 * pi, length.out = 100))
+#'   act <- RBeast::calc_act(
+#'     trace = trace,
+#'     sample_interval = 1
+#'   )
+#'   testthat::expect_equal(object = act, expected = 38.18202, tolerance = 0.01)
 #' @seealso Java code can be found here: \url{https://github.com/CompEvol/beast2/blob/9f040ed0357c4b946ea276a481a4c654ad4fff36/src/beast/core/util/ESS.java#L161}
 #' @author The original Java version of the algorithm was from Remco Bouckaert,
 #'   ported to R and adapted by Richel Bilderbeek
--- tests/testthat/test-calc_act.R
+++ tests/testthat/test-calc_act.R
@@ -1,6 +1,27 @@
 context("calc_act")
 
-test_that("calc_act: abuse", {
+test_that("calc_act use", {
+
+  trace <- sin(seq(from = 0.0, to = 2.0 * pi, length.out = 100))
+  act <- RBeast::calc_act(
+    trace = trace,
+    sample_interval = 1
+  )
+  testthat::expect_equal(object = act, expected = 38.18202, tolerance = 0.01)
+})
+
+test_that("calc_act_r use", {
+
+  trace <- sin(seq(from = 0.0, to = 2.0 * pi, length.out = 100))
+  act <- RBeast::calc_act_r(
+    trace = trace,
+    sample_interval = 1
+  )
+  testthat::expect_equal(object = act, expected = 38.18202, tolerance = 0.01)
+})
+
+
+test_that("calc_act abuse", {
 
   expect_error(
     calc_act(trace = "not numeric", sample_interval = 1),
